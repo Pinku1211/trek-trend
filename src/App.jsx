517b79bfc2131612b3a4bedfe7bacdd64c79519e
Pinku1211 <pinkukumarsarker@gmail.com>
2023-12-16 22:39:15 +0600

    implement google map

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import './App.css'
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
+import { CssBaseline, Grid } from '@material-ui/core'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CssBaseline></CssBaseline>
       <Header></Header>
-      <section>
-        <List />
-        <Map />
-      </section>
-
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
     </>
   )
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const Map = () => {
-    return (
-        <div>
-            map
-        </div>
-    );
+import mapStyles from '../../mapStyles';
+import useStyles from './styles';
+
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coordinates = {lat: 0, lng: 0};
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAIZuqVUU42LmPAnpl-IVOn_O-pUzgQUws' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >  
+      </GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
